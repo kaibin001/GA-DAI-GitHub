446a63b5bea664434e0e6ef61057cbe11164a302
kaibin001 <158435985+kaibin001@users.noreply.github.com>
2024-03-19 14:53:29 +0800

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,7 @@
+1. Go to Anaconda Prompt
+2. Execute cd your_folder_path
+3. Execute pip install -r requirements.txt
+4. Execute streamlit run app.py
+
+
+[IMPORTANT] Use files with name of image.jpg to test the app. If the file name is not image.jpg, you will get an error.
--- app.py
+++ app.py
@@ -1,53 +1,75 @@
 import os
-import sys
 import streamlit as st
 
-import openai
-from langchain.chains import ConversationalRetrievalChain
-from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import DirectoryLoader, TextLoader
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.indexes import VectorstoreIndexCreator
-from langchain.indexes.vectorstore import VectorStoreIndexWrapper
-from langchain.llms import OpenAI
-from langchain.vectorstores import Chroma
+import cv2
+import supervision as sv
 
-os.environ["OPENAI_API_KEY"] = "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"
+from tqdm import tqdm
+from inference.models.yolo_world.yolo_world import YOLOWorld
 
-# Enable to save to disk & reuse the model (for repeated queries on the same data)
-PERSIST = False
+import requests
+import numpy as np
 
-if PERSIST and os.path.exists("persist"):
-    st.write("Reusing index...\n")
-    vectorstore = Chroma(persist_directory="persist", embedding_function=OpenAIEmbeddings())
-    index = VectorStoreIndexWrapper(vectorstore=vectorstore)
-else:
-    # loader = TextLoader("data/data.txt") # Use this line if you only need a specific file e.g. data.txt
-    loader = DirectoryLoader("data/")
-    if PERSIST:
-        index = VectorstoreIndexCreator(vectorstore_kwargs={"persist_directory": "persist"}).from_loaders([loader])
-    else:
-        index = VectorstoreIndexCreator().from_loaders([loader])
 
-chain = ConversationalRetrievalChain.from_llm(
-    llm=ChatOpenAI(model="gpt-3.5-turbo"),
-    retriever=index.vectorstore.as_retriever(search_kwargs={"k": 1}),
-)
+def save_uploaded_file(uploaded_file):
+    if uploaded_file is not None:
+        with open("uploads/image.jpeg", "wb") as f:
+            f.write(uploaded_file.getbuffer())
+            
+        predict()
+        
+def predict():
+    
+    # Set image path
+    SOURCE_IMAGE_PATH = f"uploads/image.jpeg"
+    
+    # Retrieve model model
+    model = YOLOWorld(model_id="yolo_world/l")
+    
+    # Set prediction classes
+    classes = ["person", "backpack", "dog", "eye", "nose", "ear", "tongue"]
+    model.set_classes(classes)
 
-chat_history = []
+    
+    image = cv2.imread(SOURCE_IMAGE_PATH)
+    results = model.infer(image, confidence=0.003)
+    detections = sv.Detections.from_inference(results).with_nms(threshold=0.1)
+    
+    
+    BOUNDING_BOX_ANNOTATOR = sv.BoundingBoxAnnotator(thickness=2)
+    LABEL_ANNOTATOR = sv.LabelAnnotator(text_thickness=2, text_scale=1, text_color=sv.Color.BLACK)
 
-st.title("Conversational AI with Langchain and OpenAI")
-query = st.text_input("Enter a prompt:")
+    labels = [
+        f"{classes[class_id]} {confidence:0.3f}"
+        for class_id, confidence
+        in zip(detections.class_id, detections.confidence)
+    ]
 
-if st.button("Ask"):
-    if query in ['quit', 'q', 'exit']:
-        st.write("Exiting the chat.")
-        st.stop()
-    result = chain({"question": query, "chat_history": chat_history})
-    st.write(result['answer'])
-    chat_history.append((query, result['answer']))
+    annotated_image = image.copy()
+    annotated_image = BOUNDING_BOX_ANNOTATOR.annotate(annotated_image, detections)
+    annotated_image = LABEL_ANNOTATOR.annotate(annotated_image, detections, labels=labels)
+    
+    
+    
+    
+    # Show title
+    st.title("Object Detection Results")
+    
+    # Show image with annotation of predicted objects
+    annotated_image = cv2.cvtColor(np.array(annotated_image), cv2.COLOR_RGB2BGR)
+    st.image(annotated_image, use_column_width=True)
 
-st.write("Chat History:")
-for i, (input_query, answer) in enumerate(chat_history):
-    st.write(f"{i + 1}. User: {input_query}")
-    st.write(f"{i + 1}. AI: {answer}")
+def main():
+    
+    # sidebar title
+    st.sidebar.title('Object Detection using YOLO World')
+
+    uploaded_file = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+
+    if st.sidebar.button("Predict Objects") and uploaded_file is not None:
+        save_uploaded_file(uploaded_file)
+
+if __name__ == "__main__":
+    if not os.path.exists("uploads"):
+        os.makedirs("uploads")
+    main()
